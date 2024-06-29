e6086a4765c6548fea8ea4bc6dac847ed4b898e7
nastya-papulova <papulova.nastya2001@gmail.com>
2024-06-29 11:52:51 +0300

    add and implement make_data.py to generate and save data

--- 1_matrix-vector_multiplication.py
+++ 1_matrix-vector_multiplication.py
@@ -1,1 +1,8 @@
-from mpi4py import
+from mpi4py import MPI
+from numpy import empty
+
+comm = MPI.COMM_WORLD
+numprocs = comm.Get_size()
+rank = comm.Get_rank()
+
+print("hello from process {0} out of {1}".format(rank, numprocs))
