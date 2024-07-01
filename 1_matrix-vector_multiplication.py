42dd2ee2663ddfe2a1f0fa556897cb450f5fca33
nastya-papulova <papulova.nastya2001@gmail.com>
2024-07-01 20:21:31 +0300

    Created matrix-vector multiplication script
    - Taken into account shape mismatch in parallel_multiplication function
    - Added comments and documentation to clarify code functionality
    - Ensured regular and parallel multiplication results match
    - Comparison of running time of regular and parallel multiplication
    - Updated comments in make_data.py

--- 1_matrix-vector_multiplication.py
+++ 1_matrix-vector_multiplication.py
@@ -1,8 +1,215 @@
-from mpi4py import MPI
-from numpy import empty
+import re
+import time
 
+import numpy as np
+from mpi4py import MPI
+
+
+def read_data_info(filename):
+    """
+    Reads the size of matrix A and the length of vector x from a data file.
+
+    Parameters:
+    filename (str): The path to the data file.
+
+    Returns:
+    tuple: A tuple of two integers containing the size of matrix A and the integer length of vector x.
+
+    Raises:
+    ValueError: If vector and matrix cannot be multiplied due to incorrect dimensions.
+    ValueError: If the expected data format is not found in the file.
+    """
+    with open(filename, "r") as file:
+        content = file.read()
+
+    matrix_A_size_match = re.search(r"matrix_A_size\s*=\s*\((\d+),\s*(\d+)\)", content)
+    vector_x_len_match = re.search(r"vector_x_len\s*=\s*(\d+)", content)
+
+    if matrix_A_size_match and vector_x_len_match:
+        matrix_A_size = (
+            int(matrix_A_size_match.group(1)),
+            int(matrix_A_size_match.group(2)),
+        )
+        vector_x_len = int(vector_x_len_match.group(1))
+        if vector_x_len != matrix_A_size[1]:
+            raise ValueError(
+                "Vector and matrix cannot be multiplied due to incorrect dimensions."
+            )
+
+        return matrix_A_size, vector_x_len
+
+    else:
+        raise ValueError("The expected data format was not found in the file.")
+
+
+def sequential_multiplication(
+    matrix_A_size,
+    vector_x_len,
+    matrix_A_data_filename,
+    vector_x_data_filename,
+):
+    """
+    Performs regular (sequential) matrix-vector multiplication.
+
+    Parameters:
+    matrix_A_size (tuple): The size of matrix A.
+    vector_x_len (int): The length of vector x.
+    matrix_A_data_filename (str): The filename containing the data for matrix A.
+    vector_x_data_filename (str): The filename containing the data for vector x.
+
+    Returns:
+    np.ndarray: The resulting vector after multiplication.
+    """
+
+    matrix_A = np.loadtxt(matrix_A_data_filename).reshape(matrix_A_size)
+    vector_x = np.loadtxt(vector_x_data_filename)
+
+    b = np.dot(matrix_A, vector_x)
+
+    return b
+
+
+def parallel_multiplication(
+    comm,
+    numprocs,
+    rank,
+    matrix_A_size,
+    vector_x_len,
+    matrix_A_data_filename,
+    vector_x_data_filename,
+):
+    """
+    Performs parallel matrix-vector multiplication using MPI.
+
+    Parameters:
+    comm (MPI.Comm): The MPI communicator.
+    numprocs (int): The number of processes.
+    rank (int): The rank of the current process.
+    matrix_A_size (tuple): The size of matrix A.
+    vector_x_len (int): The length of vector x.
+    matrix_A_data_filename (str): The filename containing the data for matrix A.
+    vector_x_data_filename (str): The filename containing the data for vector x.
+
+    Returns:
+    np.ndarray: The resulting vector after multiplication (only for the root process).
+    """
+
+    if rank == 0:
+        M, N = matrix_A_size
+        M, N = np.array(M), np.array(N)
+    else:
+        N = np.array(0, dtype=np.int32)
+
+    # Broadcast the number of columns in matrix A to all processes
+    comm.Bcast([N, 1, MPI.INT], root=0)
+
+    # Generate arrays rcounts - stores the lengths of parts of the array on each process and displs - stores the indexes of the first elements
+    if rank == 0:
+        displs = np.empty(numprocs, dtype=np.int32)
+        rcounts = np.empty(numprocs, dtype=np.int32)
+        displs[0] = 0
+        rcounts[0] = 0
+        ave, res = divmod(M, numprocs - 1)
+        rcounts[1:] = [int(ave + 1 if i <= res else ave) for i in range(1, numprocs)]
+        displs[1:] = np.cumsum(rcounts[:-1])
+    else:
+        rcounts = None
+        displs = None
+
+    # Broadcast the scatter counts and displacements to all processes
+    rcounts = comm.bcast(rcounts if rank == 0 else None, root=0)
+    displs = comm.bcast(displs if rank == 0 else None, root=0)
+
+    # Each process gets the number of rows it will handle
+    M_part = np.array(0, dtype=np.int32)
+    comm.Scatterv(
+        [rcounts, np.ones(numprocs), np.arange(numprocs), MPI.INT],
+        [M_part, 1, MPI.INT],
+        root=0,
+    )
+
+    # Allocate space for the part of matrix A each process will handle
+    A_part = np.empty((M_part, N), dtype=np.float64)
+
+    if rank == 0:
+        matrix_A = np.empty(matrix_A_size, dtype=np.float64)
+        matrix_A = np.loadtxt(matrix_A_data_filename).reshape(matrix_A_size)
+
+        # Scatter the parts of matrix A to all processes
+        comm.Scatterv(
+            [matrix_A, rcounts * N, displs * N, MPI.DOUBLE],
+            [A_part, M_part * N, MPI.DOUBLE],
+            root=0,
+        )
+
+    else:
+        comm.Scatterv(
+            [None, None, None, None], [A_part, M_part * N, MPI.DOUBLE], root=0
+        )
+
+    # Allocate space for the vector x
+    vector_x = np.empty(vector_x_len, dtype=np.float64)
+
+    if rank == 0:
+        vector_x = np.loadtxt(vector_x_data_filename)
+
+    # Broadcast the vector x to all processes
+    comm.Bcast([vector_x, N, MPI.DOUBLE], root=0)
+
+    b_part = np.dot(A_part, vector_x)
+
+    # Each process computes its part of the result
+    if rank == 0:
+        b = np.empty(M, dtype=np.float64)
+    else:
+        b = None
+
+    # Gather the parts of the result from all processes
+    comm.Gatherv(
+        [b_part, rcounts[rank], MPI.DOUBLE], [b, rcounts, displs, MPI.DOUBLE], root=0
+    )
+
+    return b
+
+
+data_info_filename = "Data/data_info.dat"
+vector_x_data_filename = "Data/vector_x.dat"
+matrix_A_data_filename = "Data/matrix_A.dat"
+
+matrix_A_size, vector_x_len = read_data_info(data_info_filename)
+
+# Initialize MPI
 comm = MPI.COMM_WORLD
 numprocs = comm.Get_size()
 rank = comm.Get_rank()
 
-print("hello from process {0} out of {1}".format(rank, numprocs))
+start_time = time.time()
+
+b_parallel = parallel_multiplication(
+    comm,
+    numprocs,
+    rank,
+    matrix_A_size,
+    vector_x_len,
+    matrix_A_data_filename,
+    vector_x_data_filename,
+)
+end_time = time.time()
+
+if rank == 0:
+    print(f"Parallel multiplication time: {end_time - start_time} seconds")
+
+
+start_time = time.time()
+
+b_sequential = sequential_multiplication(
+    matrix_A_size,
+    vector_x_len,
+    matrix_A_data_filename,
+    vector_x_data_filename,
+)
+end_time = time.time()
+
+if rank == 0:
+    print(f"Sequential multiplication time: {end_time - start_time} seconds")
+    print(np.allclose(b_parallel, b_sequential))
--- make_data.py
+++ make_data.py
@@ -1,38 +1,43 @@
-import numpy as np
 import os
 
-matrix_A_size = (20, 30)
-vector_x_len = 30
+import numpy as np
+
+matrix_A_size = (1500, 1000)
+vector_x_len = 1000
+
 
 def write_data_info(matrix_A_size=matrix_A_size, vector_x_len=vector_x_len):
-    # Writes info about data files in a file.
-    os.makedirs('Data', exist_ok=True)
+    # Writes info about data files in a file
+    os.makedirs("Data", exist_ok=True)
     try:
-        with open('Data/data_info.dat', 'w') as file:
-            file.write(f"matrix_A_size = {matrix_A_size}\nvector_x_len = {vector_x_len}")
+        with open("Data/data_info.dat", "w") as file:
+            file.write(f"matrix_A_size={matrix_A_size}\nvector_x_len={vector_x_len}")
     except FileNotFoundError as e:
-        print(f'Error writing to file {file}: {e}')    
+        print(f"Error writing to file {file}: {e}")
 
-def write_matrix_A_data(matrix_A_size=matrix_A_size):   
-    # Generate and write a random matrix to a file, one element per line.
-    os.makedirs('Data', exist_ok=True)
+
+def write_matrix_A_data(matrix_A_size=matrix_A_size):
+    # Generate and write a random matrix to a file, one element per line
+    os.makedirs("Data", exist_ok=True)
     M, N = matrix_A_size
     try:
-        with open('Data/matrix_A.dat', 'w') as file:
+        with open("Data/matrix_A.dat", "w") as file:
             matrix_A = np.random.random_sample(matrix_A_size)
-            file.write("\n".join(map(str, matrix_A.reshape(M*N))))
+            file.write("\n".join(map(str, matrix_A.reshape(M * N))))
     except FileNotFoundError as e:
-        print(f'Error writing to file {file}: {e}') 
+        print(f"Error writing to file {file}: {e}")
 
-def write_vector_x_data(vector_x_len=vector_x_len): 
-    # Generate and write a random vector to a file, one element per line.
-    os.makedirs('Data', exist_ok=True) 
+
+def write_vector_x_data(vector_x_len=vector_x_len):
+    # Generate and write a random vector to a file, one element per line
+    os.makedirs("Data", exist_ok=True)
     try:
-        with open('Data/vector_x.dat', 'w') as file:
+        with open("Data/vector_x.dat", "w") as file:
             vector_x = np.random.random_sample(vector_x_len)
             file.write("\n".join(map(str, vector_x)))
     except FileNotFoundError as e:
-        print(f'Error writing to file {file}: {e}') 
+        print(f"Error writing to file {file}: {e}")
+
 
 write_data_info(matrix_A_size=matrix_A_size, vector_x_len=vector_x_len)
 write_matrix_A_data(matrix_A_size=matrix_A_size)
