54cee3db314aac9ddd7f856dc833f040829bfdfc
nastya-papulova <papulova.nastya2001@gmail.com>
2024-07-06 18:58:16 +0300

    Add function get_sine_data - a function has been added that allows you to obtain data for an equation of the form Ax=b, where A is a matrix, x is a vector, the values ​​of which on the graph are determined by a sinusoid, b is a vector

--- make_data.py
+++ make_data.py
@@ -4,7 +4,7 @@ import re
 import numpy as np
 
 matrix_size = (1500, 1000)
-vector_len = 1500
+vector_len = 1000
 
 
 def write_data_info(filename, explanatory_line, data_info):
@@ -55,8 +55,28 @@ def write_vector_data(filename, vector_len):
         print(f"Error writing to file {file}: {e}")
 
 
-# write_data_info(filename='Data/data_info.dat', explanatory_line='matrix_A_size=', data_info=matrix_size)
-# write_matrix_data(filename='Data/matrix_A.dat', matrix_size=matrix_size)
+def write_sine_data(matrix_filename, vector_filename, matrix_size):
+    # Generates a random matrix and a sine vector, computes their dot product, and writes both to files
+    os.makedirs("Data", exist_ok=True)
+
+    M, N = matrix_size
+    matrix = np.random.random_sample(matrix_size)
+    t = np.linspace(0, 1, N, endpoint=False)
+    sin = np.sin(2 * np.pi * N * t)
+    vector = np.dot(matrix, sin)
+
+    try:
+        with open(matrix_filename, "w") as file:
+            file.write("\n".join(map(str, matrix.reshape(M * N))))
+    except FileNotFoundError as e:
+        print(f"Error writing to file {file}: {e}")
+
+    try:
+        with open(vector_filename, "w") as file:
+            file.write("\n".join(map(str, vector)))
+    except FileNotFoundError as e:
+        print(f"Error writing to file {file}: {e}")
+
 
 """
 write_data_info(
@@ -65,17 +85,23 @@ write_data_info(
     data_info=vector_len,
 )
 write_vector_data(filename="Data/vector_y.dat", vector_len=vector_len)
-"""
+
 write_data_info(
     filename="Data/data_info.dat",
     explanatory_line="matrix_A_size=",
     data_info=matrix_size,
 )
 write_matrix_data(filename="Data/matrix_A.dat", matrix_size=matrix_size)
-
+write_vector_data(filename="Data/vector_x.dat", vector_len=vector_len)
+"""
 write_data_info(
     filename="Data/data_info.dat",
     explanatory_line="vector_x_len=",
     data_info=vector_len,
 )
-write_vector_data(filename="Data/vector_x.dat", vector_len=vector_len)
+
+write_sine_data(
+    matrix_filename="Data/matrix_A.dat",
+    vector_filename="Data/vector_x.dat",
+    matrix_size=matrix_size,
+)
