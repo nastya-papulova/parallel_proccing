1476b104eed1aa8b64778371cfccc51c3354de7c
nastya-papulova <papulova.nastya2001@gmail.com>
2024-07-07 19:34:01 +0300

    Finish the code 4_conjugate_gradient_method_SLAE.py

--- 4_conjugate_gradient_method_SLAE.py
+++ 4_conjugate_gradient_method_SLAE.py
@@ -40,6 +40,23 @@ def read_data_info(filename):
         return matrix_A_size, vector_x_len
 
 
+def sequential_calculation_conjugate_gradient_method(A, b, x, N):
+    s = 1
+    p = np.zeros(N)
+
+    while s <= N:
+        if s == 1:
+            r = np.dot(A.T, np.dot(A, x) - b)
+        else:
+            r -= q / np.dot(p, q)
+
+        p += r / np.dot(r, r)
+        q = np.dot(A.T, np.dot(A, p))
+        x -= p / np.dot(p, q)
+        s += 1
+    return x
+
+
 def sequential_conjugate_gradient_method(
     matrix_A_size,
     matrix_A_data_filename,
@@ -57,11 +74,12 @@ def sequential_conjugate_gradient_method(
     Returns:
     np.ndarray: The resulting vector of SLAE solution.
     """
-
+    M, N = matrix_A_size
     matrix_A = np.loadtxt(matrix_A_data_filename).reshape(matrix_A_size)
     vector_b = np.loadtxt(vector_b_data_filename)
 
-    x = cg(matrix_A, vector_b)
+    x = np.zeros(N)
+    x = sequential_calculation_conjugate_gradient_method(matrix_A, vector_b, x, N)
 
     return x
 
@@ -75,30 +93,54 @@ def auxiliary_arrays_determination(M, numprocs):
     rcounts[1:] = [int(ave + 1 if i <= res else ave) for i in range(1, numprocs)]
     displs[1:] = np.cumsum(rcounts[:-1])
 
+    return rcounts, displs
+
+
+def parallel_calculation_conjugate_gradient_method(A_part, b_part, x, N):
+    r = np.empty(N, dtype=np.float64)
+    q = np.empty(N, dtype=np.float64)
+
+    s = 1
+    p = np.zeros(N, dtype=np.float64)
+
+    while s <= N:
+        if s == 1:
+            r_temp = np.dot(A_part.T, np.dot(A_part, x) - b_part)
+            comm.Allreduce([r_temp, N, MPI.DOUBLE], [r, N, MPI.DOUBLE], op=MPI.SUM)
+        else:
+            r -= q / np.dot(p, q)
+        p += r / np.dot(r, r)
+        q_temp = np.dot(A_part.T, np.dot(A_part, p))
+        comm.Allreduce([q_temp, N, MPI.DOUBLE], [q, N, MPI.DOUBLE], op=MPI.SUM)
+
+        x -= p / np.dot(p, q)
+        s += 1
+    return x
+
 
 def parallel_conjugate_gradient_method(
     comm,
     numprocs,
     rank,
     matrix_A_size,
-    vector_x_len,
+    vector_b_len,
     matrix_A_data_filename,
-    vector_x_data_filename,
+    vector_b_data_filename,
 ):
     """
-    Performs parallel transposed matrix-vector multiplication using MPI.
+    Performs parallel conjugate gradient method for solving SLAEs.
 
     Parameters:
     comm (MPI.Comm): The MPI communicator.
     numprocs (int): The number of processes.
     rank (int): The rank of the current process.
     matrix_A_size (tuple): The size of matrix A.
-    vector_x_len (int): The length of vector x.
+    vector_b_len (int): The length of vector b.
     matrix_A_data_filename (str): The filename containing the data for matrix A.
-    vector_x_data_filename (str): The filename containing the data for vector x.
+    vector_b_data_filename (str): The filename containing the data for vector b.
 
     Returns:
-    np.ndarray: The resulting vector after multiplication (only for the root process).
+    np.ndarray: The resulting vector of SLAE solution.
     """
 
     if rank == 0:
@@ -112,14 +154,8 @@ def parallel_conjugate_gradient_method(
 
     if rank == 0:
         rcounts_M, displs_M = auxiliary_arrays_determination(M, numprocs)
-        rcounts_N, displs_N = auxiliary_arrays_determination(N, numprocs)
     else:
         rcounts_M, displs_M = None, None
-        rcounts_N = np.empty(numprocs, dtype=np.int32)
-        displs_N = np.empty(numprocs, dtype=np.int32)
-
-    comm.Bcast([rcounts_N, numprocs, MPI.INT], root=0)
-    comm.Bcast([displs_N, numprocs, MPI.INT], root=0)
 
     M_part = np.array(0, dtype=np.int32)
     comm.Scatter([rcounts_M, 1, MPI.INT], [M_part, 1, MPI.INT], root=0)
@@ -139,4 +175,63 @@ def parallel_conjugate_gradient_method(
         A_part = np.empty((M_part, N), dtype=np.float64)
         comm.Recv([A_part, MPI.DOUBLE], source=0, tag=0)
 
-    return 0
+    # Allocate space for the vector b
+    vector_b = np.empty(vector_b_len, dtype=np.float64)
+
+    if rank == 0:
+        vector_b = np.loadtxt(vector_b_data_filename)
+    else:
+        vector_b = None
+
+    b_part = np.empty(M_part, dtype=np.float64)
+    comm.Scatterv(
+        [vector_b, rcounts_M, MPI.DOUBLE], [b_part, M_part, MPI.DOUBLE], root=0
+    )
+
+    x = np.zeros(N, dtype=np.float64)
+
+    x = parallel_calculation_conjugate_gradient_method(A_part, b_part, x, N)
+
+    return x
+
+
+data_info_filename = "Data/data_info.dat"
+vector_b_data_filename = "Data/vector_x.dat"
+matrix_A_data_filename = "Data/matrix_A.dat"
+
+matrix_A_size, vector_b_len = read_data_info(data_info_filename)
+
+# Initialize MPI
+comm = MPI.COMM_WORLD
+numprocs = comm.Get_size()
+rank = comm.Get_rank()
+
+start_time = time.time()
+
+x_parallel = parallel_conjugate_gradient_method(
+    comm,
+    numprocs,
+    rank,
+    matrix_A_size,
+    vector_b_len,
+    matrix_A_data_filename,
+    vector_b_data_filename,
+)
+end_time = time.time()
+
+if rank == 0:
+    print(f"Parallel conjugate gradient method time: {end_time - start_time} seconds")
+
+
+start_time = time.time()
+
+x_sequential = sequential_conjugate_gradient_method(
+    matrix_A_size,
+    matrix_A_data_filename,
+    vector_b_data_filename,
+)
+end_time = time.time()
+
+if rank == 0:
+    print(f"Sequential conjugate gradient method time: {end_time - start_time} seconds")
+    print(np.allclose(x_parallel, x_sequential))
